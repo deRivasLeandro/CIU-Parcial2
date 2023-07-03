c01747240977ae22eb43650d19cd4e4e22eb8379
Gabriel de Rivas <gabriel.derivas@estudiantes.unahur.edu.ar>
2023-07-03 01:00:46 -0300

    Proyecto funcionalidades basicas implementadas, sin estilos

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import walter_white from '../img/bb_walter_white.png';
+import jesse_pinkman from '../img/bb_jesse_pinkman.png';
 
 const Quotes = () => {
     //INICIAMOS EL LOCAL STORAGE
@@ -31,12 +33,18 @@ const Quotes = () => {
     //Definición del objeto elementos que va a contener los ids y clases de nuestros elementos
     const elementos = {
         H3FraseDePersonaje: "#frase",
-        H3UltimasFrases: "#ultimasFrases"
+        H3UltimasFrases: "#ultimasFrases",
+        Recomendacion1: "#recomendacion1",
+        Recomendacion2: "#recomendacion2",
+        Recomendacion3: "#recomendacion3",
     }
 
     //Definición de constantes que referencian elementos html
     const mostradorDeFrases = document.querySelector(elementos.H3FraseDePersonaje);
     const mostradorDeUltimasFrases = document.querySelector(elementos.H3UltimasFrases);
+    const recomendacion1Button = document.querySelector(elementos.Recomendacion1);
+    const recomendacion2Button = document.querySelector(elementos.Recomendacion2);
+    const recomendacion3Button = document.querySelector(elementos.Recomendacion3);
 
     //HOOKS
     //Acá se almacena el nombre del campeón a consultar
@@ -75,7 +83,7 @@ const Quotes = () => {
         let ultimasFrases = localStorage.getItem('citas')
         ultimasFrases = ultimasFrases.slice(1, ultimasFrases.length-2).split(`"`).filter(frase => frase !== `,`)
         if(ultimasFrases.length >= 4) {
-            ultimasFrases = ultimasFrases.slice(-3)
+            ultimasFrases = ultimasFrases.slice(-4)
             mostradorDeUltimasFrases.innerHTML = `
                 "${ultimasFrases[1]}"
                 <br>
@@ -101,7 +109,29 @@ const Quotes = () => {
         }
 
     }
-    
+
+    //Escribe el nombre de la opción en el input
+    const agregarOpcion = (opcion) => {
+        document.querySelector("#personajeInput").value = opcion;
+        editarNombre(opcion)
+    } 
+
+    //Función que genera las recomendaciones
+    const generarRecomendaciones = async() => {
+            var opciones = await consultarPersonajesDisponibles();
+            var recomendacion1 = opciones[0];
+            var recomendacion2 = opciones[1];
+            var recomendacion3 = opciones[2];
+
+            document.querySelector(elementos.Recomendacion1).innerHTML = recomendacion1;
+            document.querySelector(elementos.Recomendacion1).onclick = () => { agregarOpcion(recomendacion1); };
+
+            document.querySelector(elementos.Recomendacion2).innerHTML = recomendacion2;
+            document.querySelector(elementos.Recomendacion2).onclick = () => { agregarOpcion(recomendacion2); };
+
+            document.querySelector(elementos.Recomendacion3).innerHTML = recomendacion3;
+            document.querySelector(elementos.Recomendacion3).onclick = () => { agregarOpcion(recomendacion3); };
+    }
 
     // //Función para actualizar las últimas frases vistas
     // const actualizarUltimasFrases = () => {
@@ -147,9 +177,26 @@ const Quotes = () => {
         mostradorDeUltimasFrases.innerHTML = ultimasFrases;
     }
     */
+    try {
+        setTimeout(() => {
+            if(document.querySelector(elementos.Recomendacion1).innerHTML === "") {
+                generarRecomendaciones();
+            }
+        }, 1)
+        
+    } catch (e) {
+        console.log(e)
+    }
 
     return (  
         <> 
+            <h3>Seleccione el personaje del cual desea leer una frase o escriba su nombre más abajo:</h3>
+            <img src={walter_white} alt="Walter White" />
+            <img src={jesse_pinkman} alt="Jesse Pinkman" />
+            <img src="../../img/bb_saul_goodman" alt="Saul Goodman" />
+            <img src="../../img/bb_gus_fring" alt="Gus Fring" />
+            <img src="../../img/bb_mike_ehrmantraut" alt="Mike Ehrmantraut" />
+            <img src="../../img/bb_hank_schrader" alt="Hank Schrader" />
             <h3>Escriba el nombre del personaje del que quiera leer una frase:</h3>
             <input 
                 type="text" 
@@ -162,6 +209,12 @@ const Quotes = () => {
             >Buscar Frases
             </button>
             <br />
+            <h5>O elegí una de nuestras recomendaciones:</h5>
+            <ul>
+                <li><button id="recomendacion1"></button></li>
+                <li><button id="recomendacion2"></button></li>
+                <li><button id="recomendacion3"></button></li>
+            </ul>
             <h3>Frase:</h3>
             <h3 id="frase">No has solicitado ninguna frase todavía.</h3>
             <h3>Últimas frases vistas:</h3>
